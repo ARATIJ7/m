bfded50acee4b3a5e2e83faf5ffef11bf1ec130c
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-14 16:46:51 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "east_instance_public_ip" {
-  value = aws_instance.east_mongodb_instance.public_ip
-}
-
-output "west_instance_public_ip" {
-  value = aws_instance.west_mongodb_instance.public_ip
-}
