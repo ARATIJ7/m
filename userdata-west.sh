79f3b95d0a0a4e5b5a0fe5bb445170bca164e5eb
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-15 14:10:05 +0530

    Update userdata-west.sh

--- userdata-west.sh
+++ userdata-west.sh
@@ -1,27 +1,24 @@
-#!/bin/bash
-# Install MongoDB
-sudo apt-get update
-sudo apt-get install -y mongodb
-
-# Configure MongoDB replica set
-cat <<EOF > /etc/mongod.conf
-replication:
-  replSetName: "rs0"
-net:
-  bindIp: 0.0.0.0
-EOF
-
-# Start MongoDB
-sudo systemctl start mongodb
-
-# Enable MongoDB service
-sudo systemctl enable mongodb
-
-# Wait for MongoDB to start
-sleep 10
-
-# Connect to the East instance and add this instance to the replica set
-# Replace <East-Instance-Public-IP> with the actual public IP of the East instance
-
-east_ip="<East-Instance-Public-IP>"
-mongo --host ${east_ip}:27017 --eval 'rs.add("'$(curl http://169.254.169.254/latest/meta-data/public-ipv4)':27017")'
+user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y mongodb
+              mkdir -p /data/db
+              cat <<EOT > /etc/mongod.conf
+              systemLog:
+                destination: file
+                logAppend: true
+                path: /var/log/mongodb/mongod.log
+              storage:
+                dbPath: /var/lib/mongodb
+                journal:
+                  enabled: true
+              net:
+                bindIp: 0.0.0.0
+                port: 27017
+              replication:
+                replSetName: "rs0"
+              EOT
+              systemctl start mongod
+              sleep 30
+              mongo --eval 'rs.add("<mongo_east_instance_ip>:27017")'
+              EOF
