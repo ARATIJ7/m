a5ee63dbc3a71e2625cc131f43c3405b9c05234c
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-19 17:29:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -180,7 +180,7 @@ resource "aws_security_group" "west_mongodb_sg" {
   }
 }
 
-resource "aws_instance" "west_mongodb_instance" {
+resource "aws_instance" "west_mongodb_instance_1" {
   provider = aws.west
   ami = var.ami_id_west
   instance_type = var.instance_type
@@ -192,7 +192,23 @@ resource "aws_instance" "west_mongodb_instance" {
   user_data = file("userdata-west.sh")
 
   tags = {
-    Name = "West-MongoDB-Instance"
+    Name = "West-MongoDB-Instance-1"
+  }
+}
+
+resource "aws_instance" "west_mongodb_instance_2" {
+  provider = aws.west
+  ami = var.ami_id_west
+  instance_type = var.instance_type
+  key_name = var.key_name
+  subnet_id = aws_subnet.west_subnet.id
+  vpc_security_group_ids = [aws_security_group.west_mongodb_sg.id]
+  associate_public_ip_address = true
+
+  user_data = file("userdata-west.sh")
+
+  tags = {
+    Name = "West-MongoDB-Instance-2"
   }
 }
 
@@ -200,6 +216,10 @@ output "east_instance_public_ip" {
   value = aws_instance.east_mongodb_instance.public_ip
 }
 
-output "west_instance_public_ip" {
-  value = aws_instance.west_mongodb_instance.public_ip
+output "west_instance_1_public_ip" {
+  value = aws_instance.west_mongodb_instance_1.public_ip
+}
+
+output "west_instance_2_public_ip" {
+  value = aws_instance.west_mongodb_instance_2.public_ip
 }
